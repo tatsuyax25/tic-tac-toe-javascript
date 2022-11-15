e4c7d6ad3796463532315ef5cd7771699566ae96
Miguel Urena <urena_miguel82@outlook.com>
2022-11-15 14:54:15 -0800

    Add a placeMark function

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
 const cellElements = document.querySelectorAll('[data-cell]')
+let circleTurn
 
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, { once: true })
 })
 
 function handleClick(e) {
-    // placeMark
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
     // Check for Win!
     // Check for Draw!
     // Switch turns
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
 }
