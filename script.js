5fe338e3b497dde6e04a486bb06d83d17a13e4e9
Miguel Urena <urena_miguel82@outlook.com>
2022-11-16 14:58:58 -0800

    Update Draw condition

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function handleClick(e) {
 
 function endGame(draw) {
     if (draw) {
-        winningMessageElement.innerText = 'Draw!'
+        winningMessageTextElement.innerText = 'Draw!'
     } else {
         winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
     }
